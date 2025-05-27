c0d53256ba191b4bacba8a86ac3560aed03bbf19
Ravi <ravikumarit42@gmail.com>
2025-05-27 23:39:36 +0530

    initial commit

---
+++ app/src/main/java/com/pw/contactappassignment/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pw.contactappassignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
